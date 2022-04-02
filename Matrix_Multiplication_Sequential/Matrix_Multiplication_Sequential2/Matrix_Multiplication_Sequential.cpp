05db0f66a89523e2a1547c5030920dca3c557a04
d4vvv <kacper.godula99@gmail.com>
2022-04-02 18:49:27 +0200

    whatever

---
+++ Matrix_Multiplication_Sequential/Matrix_Multiplication_Sequential2/Matrix_Multiplication_Sequential.cpp
@@ -0,0 +1,37 @@
+﻿// Matrix_Multiplication_Sequential2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int matrix_width = 3;
+    const int matrix_height = 3;
+    float matrix[matrix_width][matrix_height];
+
+    for (int i = 0; i < matrix_width; i++)
+    {
+        cout << "[ ";
+        for (int j = 0; j < matrix_height; j++)
+        {
+            matrix[i][j] = rand() % 123;
+            cout << matrix[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+
+    cout << endl;
+    const int multiplier = 2;
+
+    for (int i = 0; i < matrix_width; i++)
+    {
+        cout << "[ ";
+        for (int j = 0; j < matrix_height; j++)
+        {
+            matrix[i][j] *= multiplier;
+            cout << matrix[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+}
